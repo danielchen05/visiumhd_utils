e24a6460c04f625dc46e34400112048fa695fc72
Daniel Chen <danielchenxingyi@gmail.com>
2025-06-30 15:25:38 -0400

    all test files implemented - skip read_data as it needs real VisiumHD folder - bump to version v0.2.0

--- tests/test_read_data.py
+++ tests/test_read_data.py
@@ -1,2 +1,11 @@
-
-#TODO: add tests
+"""
+The read_data.to_spatialdata() helper is essentially a wrapper of spatialdata-io,
+as well as some additional manipulations on file structures with respect to real HD data. 
+Currently, we do not have a lightweight public Visium HD dataset to implement the tests.
+Will revisit later if necessary.
+"""
+import pytest
+pytest.skip(
+    "Skipping read_data tests: wrapper depends on real Visium HD input",
+    allow_module_level=True,
+)
